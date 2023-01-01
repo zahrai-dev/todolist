6421d772475e470aaf54a63ab9c9d1bd5a916fa4
zahrai-dev <zahrai.dev@gmail.com>
2023-01-02 01:35:37 +0700

    edit-todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      todoItem: '',
+      items: []
+    }
+  }
+  
+  handleSubmit = (event) =>{
+    event.preventDefault()
+    this.setState({
+      items     : [...this.state.items, this.state.todoItem],
+      todoItem  : ''
+      // items: this.state.items.concat([this.state.todoItem])
+    })
+  }
+  handleChange = (event) =>{
+    this.setState({
+      todoItem: event.target.value
+    })
+  }
+  
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.todoItem} onChange={this.handleChange}/>
+          <button>ADD</button>
+        </form>
+        <List items={this.state.items}/>
+      </div>
+    );
+  }
 }
 
 export default App;
